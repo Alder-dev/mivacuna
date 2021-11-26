60c4e150edabf5b1034dda5e361ec5a32eccd39d
Alder-dev <72114374+Alder-dev@users.noreply.github.com>
2021-11-25 23:50:46 -0500

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -90,7 +90,6 @@ $autoload['drivers'] = array();
 |	$autoload['helper'] = array('url', 'file');
 */
 $autoload['libraries'] = array('form_validation');
-$autoload['libraries'] = array('Mongodb');
 $autoload['helper'] = array('url');
 
 /*
